321b3fc92835f26bef6099c24b2d06c7d1a44c74
Tomás Catena <tomascatena@gmail.com>
2021-02-13 19:13:46 +0100

    Initial commit

---
+++ database/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
